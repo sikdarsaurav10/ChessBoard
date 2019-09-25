570f21ed5daa8aee0a710968ef8f16f5b28b34c9
Saurav Sikdar <sikdarsaurav10@gmail.com>
2019-09-25 15:42:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# x and y are the co-ordinates of the opposite pawn
-# the output should produce a series of tuples of the path taken by our pawn
 '''
 The time complexity for the below program for the best case if no pawn encountered is -> O(n)
 and the worst case if we encounter a opp_pawn on our path is -> O(n^3)
@@ -8,11 +6,8 @@ The space complexity for the program can be stated to have a constant space comp
 So, should i try another approach for the below program and try to see if i can reduce the time
 complexity of the program!!
 '''
-
-
-
-
-
+# x and y are the co-ordinates of the opposite pawn
+# the output should produce a series of tuples of the path taken by our pawn
 x, y = [int(x) for x in input("Enter co-ordinates of pawn with space(expect(0,0) and (7,7)): ").split()]
 opp_pawn = (x,y)
 
@@ -21,13 +16,16 @@ i = 0
 j = 0
 
 flag = False
+#checking if there is a pawn on our shortest route
 while i<=8 and j<=8:
     pawn = (i,j)
+    #if pawn is encountered
     if i==x and j==y:
         flag = True
         print('Pawn encountered taking alternate route!!')
         i = 0
         j = 0
+     #if pawn is not encountered
     if flag == False:
         print(pawn)
         i+=1
